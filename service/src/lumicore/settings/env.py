10dbd17aa5ec82df6cf4bac16130463f2ccd7f3d
devdinho <freitas.dev@proton.me>
2025-05-29 02:06:39 -0300

    feat: corsheaders

--- service/src/lumicore/settings/env.py
+++ service/src/lumicore/settings/env.py
@@ -15,6 +15,8 @@ POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 DB_HOST = "lumicore_db"
 
+CORS_ALLOW_ALL_ORIGINS = bool(os.getenv("CORS_ALLOW_ALL_ORIGINS", False))
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 ALLOWED_HOSTS = [
@@ -23,7 +25,12 @@ ALLOWED_HOSTS = [
     "lumicore.dinho.dev"
 ]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8003", "http://0.0.0.0:8003", "https://lumicore.dinho.dev"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8003",
+    "http://localhost:5173",
+    "http://0.0.0.0:8003", 
+    "https://lumicore.dinho.dev"
+]
 
 CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS
 
